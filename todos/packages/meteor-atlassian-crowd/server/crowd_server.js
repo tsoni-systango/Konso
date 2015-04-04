d30002f185d1a04f6e451492f151ef460854d67c
Ivan Suslov <zuzel_tv@mail.ru>
2015-04-05 04:48:29 +0600

    Fixes

--- todos/packages/meteor-atlassian-crowd/server/crowd_server.js
+++ todos/packages/meteor-atlassian-crowd/server/crowd_server.js
@@ -13,11 +13,7 @@ var loginWithAtlassianCrowdSync = function (username, password) {
     if (!ATLASSIAN_CROWD_CONFIG.application.password) {
         throw new Meteor.Error("Malformed config", "application.password is not defined");
     }
-    _doRequest=null;
     var crowd = new AtlassianCrowd(ATLASSIAN_CROWD_CONFIG);
-    _doRequest_patch();
-    _doRequest = null;
-    console.log(_doRequest);
     var syncFuture = new Future();
     crowd.user.authenticate(username, password, function (err, res) {
             syncFuture.return({
@@ -85,103 +81,4 @@ Accounts.registerLoginHandler("atlassianCrowd", function (loginRequest) {
             token: stampedToken.token
         };
     }
-})
-
-function _doRequest_patch() {
-    _doRequest = function (options, callback) {
-        var data = "", error;
-
-        var opts = {
-            hostname: this.settings.hostname,
-            port: this.settings.port,
-            auth: this.settings.authstring,
-            method: options.method,
-            path: settings.pathname + settings.apipath + options.path,
-            headers: {
-                "Accept": "application/json"
-            }
-        };
-
-        if (options.method === "POST" || options.method === "PUT") {
-            if (options.data) {
-                opts.headers['content-type'] = "application/json";
-                opts.headers['content-length'] = options.data.length;
-            }
-            else {
-                error = new Error("Missing POST Data");
-                error.type = "BAD_REQUEST";
-                return callback(error);
-            }
-        }
-        else {
-            if (options.method === "DELETE") {
-                // nginx requires content-length header also for DELETE requests
-                opts.headers['content-length'] = '0';
-            }
-        }
-
-        var protocol = (settings.protocol == "https:") ? https : http;
-
-        var request = protocol.request(opts, function (response) {
-
-            response.on('data', function (chunk) {
-                data += chunk.toString();
-            });
-
-            if (response.statusCode === 204) {
-                return callback(null, response.statusCode);
-            }
-
-            if (response.statusCode === 401) {
-                error = new Error("Application Authorization Error");
-                error.type = "APPLICATION_ACCESS_DENIED";
-                return callback(error);
-            }
-
-            if (response.statusCode === 403) {
-                error = new Error("Application Permission Denied");
-                error.type = "APPLICATION_PERMISSION_DENIED";
-                return callback(error);
-            }
-
-            response.on('end', function () {
-                if (response.headers['content-type'] !== "application/json") {
-                    error = new Error("Invalid Response from Atlassian Crowd");
-                    error.type = "INVALID_RESPONSE";
-                    return callback(error);
-                }
-                else {
-                    if (data) {
-                        data = JSON.parse(data);
-                        if (data.reason || data.message) {
-                            if (typeof data.reason === "undefined") {
-                                data.reason = "BAD_REQUEST";
-                                data.message = "Invalid Request to Atlassian Crowd";
-                            }
-                            error = new Error(data.message);
-                            error.type = data.reason;
-                            return callback(error);
-                        }
-                        else {
-                            return callback(null, data);
-                        }
-                    }
-                    else {
-                        return callback(null, response.statusCode);
-                    }
-                }
-            });
-        });
-
-        request.on("error", function (e) {
-            return callback(e);
-        });
-
-        if (options.data) {
-            request.end(options.data);
-        }
-        else {
-            request.end();
-        }
-    };
-}
+});
