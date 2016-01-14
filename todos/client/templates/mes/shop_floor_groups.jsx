e423e55b6a547b629625805bfb8d89a381c553e4
Deepti Mota <deepti@systango.com>
2016-01-14 18:25:13 +0530

    created level 1 for shopfloorgroups

---
+++ todos/client/templates/mes/shop_floor_groups.jsx
@@ -0,0 +1,48 @@
+ShopFloorGroups = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  getInitialState: function() {
+    return {
+      showShopFloorGroups:false
+    }
+  },
+  getMeteorData : function(){
+      Meteor.subscribe('fetchShopFloorList');
+      return{
+      shopfloorList : ShopFloor.find({},{fields:{"shopfloorGroupName":1,"shopfloorGroup":1,"shopfloor":1}}).fetch()
+    }
+  },
+  expandItem : function(){
+    this.setState({ showShopFloorGroups : !this.state.showShopFloorGroups });
+  },
+  render : function(){
+  var shop_floor_groups = [];  
+  if (this.state.showShopFloorGroups) {
+    this.data.shopfloorList.map(function (element) {
+      shop_floor_groups.push(
+        <li>
+          <ShopFloorGroupList
+          shopfloorGroup={element}
+          key={element.shopfloorGroup._id}/>
+        </li>
+        )
+    });
+  }   
+    
+  return(  
+      <div>
+        Shop Floor Groups 
+        <button onClick={this.expandItem}> {!this.state.showShopFloorGroups ?"+":"-"} </button>
+        <ul>
+          {shop_floor_groups}
+        </ul>  
+      </div>
+    )
+  } 
+})
+
+Template.shop_floor_groups.helpers({
+  ShopFloorGroups() {
+    return ShopFloorGroups;
+}});
