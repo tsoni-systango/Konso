5a5cf6e0cbb3b617e7b2974b8b05bca2fee8fcb6
Deepti Mota <deepti@systango.com>
2016-02-04 21:09:01 +0530

    Moved auth check method to both dir.

--- todos/server/mes/Methods.js
+++ todos/server/mes/Methods.js
@@ -1,7 +1,8 @@
 Meteor.methods({
   savePosition : function(workcenterCode,x_coordinate,y_coordinate){
+  	console.log(IsAuthToMoveWorkCenter)
     var shopfloorgroup = ShopFloor.findOne({"shopfloor.workcenter.workcenterCode" : workcenterCode, 'shopfloor.useraccess.userId' : Meteor.user().username})
-    if (!shopfloorgroup || IsAuthToMoveWorkCenter(workcenterCode, shopfloorgroup)) {
+    if (!shopfloorgroup || !IsAuthToMoveWorkCenter(workcenterCode, shopfloorgroup)) {
       Errors.throw("Not authorized to move workcenter position.");
     };
     WorkcenterPositions.update({workcenterCode:workcenterCode}, { workcenterCode : workcenterCode, x_coordinate : x_coordinate, y_coordinate : y_coordinate }, { upsert: true });
