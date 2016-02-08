9669dd8224b3ffc286a8d95528b6325b47d1f96a
Deepti Mota <deepti@systango.com>
2016-02-08 18:18:35 +0530

    Minor fixes

--- todos/client/templates/mes/left_panel/ShopFloorRow.jsx
+++ todos/client/templates/mes/left_panel/ShopFloorRow.jsx
@@ -1,23 +1,18 @@
 ShopFloorRow = React.createClass({
   displayWorkcenters : function(){
-    DashBoardWorkCenters.remove({});
-    this.props.shopfloor.workcenter.map(function (element) {
-      
 
-      DashBoardWorkCenters.insert({
-        "workcenterCode" : element.workcenterCode,
-        "workcenterName" : element.workcenterName
-      });
-      
+    DashBoardWorkCenters.remove({});
+
+    this.props.shopfloor.workcenter.map(function (element) {
+      DashBoardWorkCenters.insert({ "workcenterCode" : element.workcenterCode, "workcenterName" : element.workcenterName });
     });
   },
+  
   render: function() {
     return (
       <li>
-        <a onClick={this.displayWorkcenters}>
-          {this.props.shopfloor.shopfloorName}
-        </a>
-        </li>
-      );
+        <a onClick={this.displayWorkcenters}> {this.props.shopfloor.shopfloorName} </a>
+      </li>
+    );
   }
 });
