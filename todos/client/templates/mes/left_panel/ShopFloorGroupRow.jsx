7531d6c2485902d2a964b21ff3c22cc2be673e5e
Deepti Mota <deepti@systango.com>
2016-02-22 21:03:46 +0530

    Updated meteor & React-tooltip implementation

--- todos/client/templates/mes/left_panel/ShopFloorGroupRow.jsx
+++ todos/client/templates/mes/left_panel/ShopFloorGroupRow.jsx
@@ -50,8 +50,6 @@ ShopFloorGroupRow = React.createClass ({
       offline: offline,
       paused: paused,
       no_data_found: no_data_found
-      // work_centers :dbWorkCenters,
-      // is_auth_for_moving : (is_auth_for_moving ? true : false)
     }
   },
   
