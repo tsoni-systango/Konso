f8da126359d94a999306c123d3c446457f2cbff8
Deepti Mota <deepti@systango.com>
2016-03-09 23:12:12 +0530

    Fixed savePosition server method error because of method name change.

--- todos/server/mes/Methods.js
+++ todos/server/mes/Methods.js
@@ -1,6 +1,5 @@
 Meteor.methods({
   savePosition : function(workcenterCode,x_coordinate,y_coordinate){
-  	console.log(IsAuthToMoveWorkCenter)
     var shopfloorgroup = ShopFloorGroup.findOne({"shopfloor.workcenter.workcenterCode" : workcenterCode, 'shopfloor.useraccess.userId' : Meteor.user().username})
     if (!shopfloorgroup || !IsAuthToAccessWorkCenter(workcenterCode, shopfloorgroup)) {
       Errors.throw("Not authorized to move workcenter position.");
