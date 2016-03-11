e4d6d1270ad01d54204302b04f26a82f9fb6e245
Deepti Mota <deepti@systango.com>
2016-03-11 13:13:44 +0530

    NaN check and check status value to chinese

--- todos/client/templates/mes/middle_dashboard/work_center_info.jsx
+++ todos/client/templates/mes/middle_dashboard/work_center_info.jsx
@@ -21,31 +21,58 @@ WorkCenterInfo = React.createClass({
 
   render : function(){
     var pos = this.getInfoDivPos();
+    var checkValue = function(value){
+      if (value){
+       return value 
+      }
+      else{
+        return " "
+      }
+    };
+    var getStatusInChinese = function(value){
+      switch (value){
+        case "FAULT":
+          return "故障"
+        case "STOP":
+          return "已停止"
+        case "OFFLINE":
+          return "離線"
+        case "PAUSE":
+          return "暫停"
+        case "WORKING":
+          return "工作中"
+        case "START":
+          return "已開始"
+        case "ONLINE":
+          return "在線"
+        }
+      return value;        
+    };
     return(
       <div className="WorkCenterInfoBox" style = {{"left": pos[0] + 'px',"top": pos[1] + 'px', "width": "260"}}>
         {this.props.info_stats.last_item ?
           <table>
             <tbody>
               <tr className="bg_dark"> <td className="wci_align_right">欄位名稱</td> <td>值</td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">狀態</td> <td> {this.props.info_stats.last_item.currentStatus} </td> </tr>
-              <tr> <td className="wci_align_right">工作中心</td> <td> {this.props.info_stats.last_item.workcenterName} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">機器名稱</td> <td> {this.props.info_stats.last_item.machineName} </td> </tr>
-              <tr> <td className="wci_align_right">工作單</td><td>  {this.props.info_stats.last_item.workorderNo} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">工序號</td> <td> {this.props.info_stats.last_item.sequenceNo} </td> </tr>
-              <tr> <td className="wci_align_right">物料編碼</td> <td> {this.props.info_stats.last_item.partno}</td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">物料名稱</td> <td> {this.props.info_stats.last_item.partnoName}</td> </tr>
-              <tr> <td className="wci_align_right">設備編號</td> <td> {this.props.info_stats.last_item.deviceNo} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">狀態</td> <td> {getStatusInChinese(this.props.info_stats.last_item.currentStatus)} </td> </tr>
+              <tr> <td className="wci_align_right">工作中心</td> <td> {checkValue(this.props.info_stats.last_item.workcenterName)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">機器名稱</td> <td> {checkValue(this.props.info_stats.last_item.machineName)} </td> </tr>
+              <tr> <td className="wci_align_right">工作單</td><td>  {checkValue(this.props.info_stats.last_item.workorderNo)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">工序號</td> <td> {checkValue(this.props.info_stats.last_item.sequenceNo)} </td> </tr>
+              <tr> <td className="wci_align_right">物料編碼</td> <td> {checkValue(this.props.info_stats.last_item.partno)}</td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">物料名稱</td> <td> {checkValue(this.props.info_stats.last_item.partnoName)}</td> </tr>
+              <tr> <td className="wci_align_right">設備編號</td> <td> {checkValue(this.props.info_stats.last_item.deviceNo)} </td> </tr>
               <tr className="bg_grey"> <td className="wci_align_right">開始時間</td> <td> {moment(this.props.info_stats.last_item.startTime).format("MMM Do h:mm:s")} </td> </tr>
               <tr> <td className="wci_align_right">最後生產時間</td> <td> {moment(this.props.info_stats.last_item.recordTime).format("MMM Do h:mm:s")} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">不良數</td> <td> {this.props.info_stats.NGCount} </td> </tr>
-              <tr> <td className="wci_align_right">累計生產數</td> <td> {this.props.info_stats.accumulativeCount} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">實際效率</td> <td> {this.props.info_stats.avg_output} </td> </tr>
-              <tr> <td className="wci_align_right">標準效率</td> <td> {this.props.info_stats.last_item.StandardWorkTime} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">當前效率</td> <td> {this.props.info_stats.currentEfficiency} </td> </tr>
-              <tr> <td className="wci_align_right">當天效率</td> <td> {this.props.info_stats.todayEfficiency} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">當前品質達標率</td> <td> {this.props.info_stats.currentQualityRate} </td> </tr>
-              <tr> <td className="wci_align_right">當天品質達標率</td> <td> {this.props.info_stats.todayQualityRate} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">功能名称</td> <td> {this.props.info_stats.last_item.functionCode} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">不良數</td> <td> {checkValue(this.props.info_stats.NGCount)} </td> </tr>
+              <tr> <td className="wci_align_right">累計生產數</td> <td> {checkValue(this.props.info_stats.accumulativeCount)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">平均輸出(小時)</td> <td> {checkValue(this.props.info_stats.avg_output)} </td> </tr>
+              <tr> <td className="wci_align_right">標準輸出(小時):</td> <td> {checkValue((3600/this.props.info_stats.last_item.StandardWorkTime).toFixed(2))} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">當前效率</td> <td> {checkValue(this.props.info_stats.currentEfficiency)} </td> </tr>
+              <tr> <td className="wci_align_right">當天效率</td> <td> {checkValue(this.props.info_stats.todayEfficiency)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">當前品質達標率</td> <td> {checkValue(this.props.info_stats.currentQualityRate)} </td> </tr>
+              <tr> <td className="wci_align_right">當天品質達標率</td> <td> {checkValue(this.props.info_stats.todayQualityRate)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">功能名称</td> <td> {checkValue(this.props.info_stats.last_item.functionName)} </td> </tr>
             </tbody>
           </table>
         :""}
